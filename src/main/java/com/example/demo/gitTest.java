8655c158c8d25b200b16b66a7ba9662b0cffa4d1
seoljuyeon <sxxljxxxxn@gmail.com>
2023-08-13 06:36:34 +0900

    :sparkles: 또 커밋 하고

--- src/main/java/com/example/demo/gitTest.java
+++ src/main/java/com/example/demo/gitTest.java
@@ -4,5 +4,7 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("잠들기 직전에");
         System.out.println("깃 테스트 갈기는 중");
+
+        System.out.println("또 커밋 하고");
     }
 }
