698c8ec97e83ecd3bd1a0aaf90a3381e7eac48d7
seoljuyeon <sxxljxxxxn@gmail.com>
2023-08-27 15:20:38 +0900

    :sparkles: 커밋 2

--- src/main/java/com/example/demo/AmendTest.java
+++ src/main/java/com/example/demo/AmendTest.java
@@ -3,5 +3,7 @@ package com.example.demo;
 public class AmendTest {
     public static void main(String[] args) {
         System.out.println("한 번 커밋 하구용");
+
+        System.out.println("다시 또 여기를 고친담에 Amend 하면??");
     }
 }
